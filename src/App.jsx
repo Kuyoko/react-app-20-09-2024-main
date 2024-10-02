bbca488a98191895c968e4705850501fe861cb8d
Kiruha <klujkokirill@gmail.com>
2024-10-02 19:03:07 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TaskList from "./components/Taskslist/TaskList";
+
+
+function App() {
+  
+  return (
+    <>
+<TaskList/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import styles from './Task.module.css'
+const Task = (props) => {
+  const {
+    dataTask: { text, days, isDone }, setDoneTask
+  } = props;
+  const setDone = () => setDoneTask()
+  return (
+    <div className={styles.task}>
+      <p style={{ color: isDone ? 'green' : 'red' }}>{text}</p>
+      <p>днів на виконання: {days + 1}</p>
+      <button onClick={setDone}>виконано</button>
+    </div>
+  );
+};
+
+Task.propTypes = {
+  dataTask: PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string.isRequired,
+    days: PropTypes.number.isRequired,
+    isDone: PropTypes.bool,
+  }),
+};
+
+export default Task;
